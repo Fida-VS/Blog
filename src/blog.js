f4a57457c526845a0d22bb16a9dc65aa9ea0f008
Fida-VS <poletaeva69vera@mail.ru>
2023-11-05 00:03:15 +0300

    Реализовать футер

--- src/blog.js
+++ src/blog.js
@@ -1,5 +1,5 @@
 import { Routes, Route } from 'react-router-dom';
-import { Header } from './components';
+import { Header, Footer } from './components';
 import styled from 'styled-components';
 
 const AppColumn = styled.div`
@@ -20,7 +20,6 @@ const H2 = styled.h2`
 	text-align: center;
 `;
 
-const Footer = () => <div>Футер</div>;
 
 export const Blog = () => {
 	return (
